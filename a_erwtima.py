66ff53c60c2f578fc6bb7643239fe0864e0fa1cd
pourpourr <giorgospour7@gmail.com>
2022-01-13 19:01:41 +0200

    Update a_erwtima.py

--- a_erwtima.py
+++ a_erwtima.py
@@ -9,6 +9,7 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.layers import *
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import QuantileTransformer
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
@@ -53,33 +54,37 @@ model.add(LSTM(units = 50))
 model.add(Dropout(0.2))# Adding the output layer
 model.add(Dense(units = 1))
 
-# Compiling the RNN
 model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+train = df.iloc[:,1:trainsize].values
+# Feature Scaling
+#sc = MinMaxScaler(feature_range = (0, lag))
+#training_set_scaled = sc.fit_transform(train.reshape(-1,1))
+
+qt = QuantileTransformer()
+training_set_scaled=qt.fit_transform(train.reshape(-1,1))
+# Compiling the RNN
 lag =1
-for j in range(n):
-    train = df.iloc[j,1:trainsize].values
+for j in range(1,n+1):
     name=df.iloc[j,0:1].values
-    # Feature Scaling
-    sc = MinMaxScaler(feature_range = (0, lag))
-    training_set_scaled = sc.fit_transform(train.reshape(-1,1))
+
     # Creating a data structure with 60 time-steps and 1 output
     X_train = []
     y_train = []
     for i in range(window, trainsize-1):
-        X_train.append(training_set_scaled[i-window:i,0])
-        y_train.append(training_set_scaled[i,0])
+        X_train.append(training_set_scaled[j*i-window:j*i,0])
+        y_train.append(training_set_scaled[i*j,0])
     X_train, y_train = np.array(X_train), np.array(y_train)
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     # Fitting the RNN to the Training set
-    print(X_train.shape[1])
-    model.fit(X_train, y_train, epochs = 2, batch_size = 32)
+    model.fit(X_train, y_train, epochs = 5, batch_size = 32)
 
     dataset_train = df.iloc[j,1:trainsize]#:
     dataset_test = df.iloc[j,trainsize:]#:
     dataset_total = pd.concat((dataset_train, dataset_test), axis = 1)
     input= df.iloc[j,colls-testsize-window:].values#1
     input=input.reshape(-1,1)
-    input=sc.transform(input)
+    input=qt.transform(input)
     x_test=[]
     for i in range(window,testsize+window+1):
         x_test.append(input[i-window:i,0])
@@ -87,7 +92,7 @@ for j in range(n):
     x_test=np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
 
     predicted_stock_price = model.predict(x_test)
-    predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+    predicted_stock_price = qt.inverse_transform(predicted_stock_price)
     dataset_test=dataset_test.to_numpy().reshape(-1,1)
 
     plt.plot(range(len(dataset_test)),dataset_test[:,0], color = "green", label = "Real "+name+ " Stock Price")
